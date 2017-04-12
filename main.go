d4a7745706ed3692a88c9cd35cf40273dec8eb62
yhy <yhy>
2017-04-12 20:49:40 +0800

    解决 1.8 启动不了 问题。

--- main.go
+++ main.go
@@ -1,15 +1,35 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"io"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "golang docker demo. Hi there, I love %s!", r.URL.Path[1:])
-}
-
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	// subscribe to SIGINT signals
+	stopChan := make(chan os.Signal)
+	signal.Notify(stopChan, os.Interrupt)
+	mux := http.NewServeMux()
+	mux.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Finished! : "+r.URL.Path[1:])
+	}))
+	srv := &http.Server{Addr: "0.0.0.0:8080", Handler: mux}
+	go func() {
+		// service connections
+		if err := srv.ListenAndServe(); err != nil {
+			log.Printf("listen: %s\n", err)
+		}
+	}()
+	<-stopChan // wait for SIGINT
+	log.Println("Shutting down server...")
+	// shut down gracefully, but wait no longer than 5 seconds before halting
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	srv.Shutdown(ctx)
+	log.Println("Server gracefully stopped")
+
 }
